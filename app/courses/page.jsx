376619a12cb35720c098f0f73f2da70e09584f3a
stupendo1238 <mohitagarwal4567@example.com>
2023-06-08 22:37:12 +0530

    Courses page designed

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import styles from "styles/Page.module.css"
 const Courses = () => {
   return (
     <div>
-      <h1 className='mt-3 font-bold text-5xl text-left mb-5'>
-        <span className='blue_gradient'>Our Courses will launch soon</span>
+      <img className={styles.courseimage} src="/assets/images/maintenance.png" alt="coming-soon"></img>
+      <h1 className='mt-5 font-semibold text-4xl'>
+        <p className='text-center text-sky-700'>Our Courses will be Launched soon</p>
+        <p className='text-center text-sky-700'>Stay Tuned !!</p>
       </h1>
     </div>
   )
