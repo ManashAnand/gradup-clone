95045b3848696ba66098855201a9b820b70513be
stupendo1238 <mohitagarwal4567@example.com>
2023-06-11 11:47:39 +0530

    Home Page

--- components/BeMentorWithUs.jsx
+++ components/BeMentorWithUs.jsx
@@ -24,8 +24,8 @@ export default function BeMentorWithUs() {
     }
   }
   return (
-    <div className="mb-8">
-    <Card css={{ p: "$6" }}>
+    <div className="mb-20">
+    <Card css={{ p: "$6",backgroundColor:"$cyan400" }}>
       <Card.Header>
         <Grid.Container css={{ pl: "$6" }}>
           {/* <Grid xs={12}>
@@ -36,15 +36,18 @@ export default function BeMentorWithUs() {
           {/* <Grid xs={12}>
             <Text css={{ color: "$accents8" }}>www.talentconnects.com</Text>
           </Grid> */}
-          <form onSubmit={handleSubmit} className="mt-6">
-            <div className="mb-2 font-bold text-4xl">
+          <form onSubmit={handleSubmit} className="mt-4">
+            <div className="mb-6 font-bold text text-4xl">
                 <h2>Be a Mentor</h2>
+                <Text css={{mt:"$4",fontSize:"$lg",fontWeight:"$normal"}}>
+                Join our team as a mentor and get to work with the sharpest minds
+            </Text>
             </div>
             <div className="mb-2">
                 <label
-                    className="block text-sm font-semibold text-gray-800"
+                    className="block text-sm text text-gray-800"
                 >
-                    Your Name
+                    Your Name *
                 </label>
                 <input 
                     type="text" 
@@ -53,9 +56,9 @@ export default function BeMentorWithUs() {
             </div>
           <div className="mb-2">
                 <label
-                    className="block text-sm font-semibold text-gray-800"
+                    className="block text-sm text text-gray-800"
                 >
-                    Company Name
+                    Company Name *
                 </label>
                 <input 
                     type="text" 
@@ -65,9 +68,9 @@ export default function BeMentorWithUs() {
             <div className="mb-4">
               <label
                 htmlFor="email"
-                className="block text-sm font-semibold text-gray-800"
+                className="block text-sm text text-gray-800"
               >
-                Email
+                Email *
               </label>
               <input
                 type="email"
@@ -76,21 +79,18 @@ export default function BeMentorWithUs() {
             </div>
             <div className="mb-2">
                 <label
-                    className="block text-sm font-semibold text-gray-800"
+                    className="block text text-sm text-gray-800"
                 >
-                    Phone Number
+                    Phone Number *
                 </label>
                 <input 
                     type="text" 
                     className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
                 />
             </div>
-            <Text>
-                Join our team as a mentor and get to work with the sharpest minds
-            </Text>
             <Link>
           <Button type="submit" className="mt-5">
-            Get in touch with us.
+            Get in touch with us
           </Button>
         </Link>
            
