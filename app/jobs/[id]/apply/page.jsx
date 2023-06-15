0446121db554d2ea1f61b734ac5e570e6bf65e53
stupendo1238 <mohitagarwal4567@example.com>
2023-06-15 23:41:53 +0530

    textchange

--- app/jobs/[id]/apply/page.jsx
+++ app/jobs/[id]/apply/page.jsx
@@ -50,7 +50,7 @@ const page = ({params}) => {
         <form onSubmit={createApplication} className='w-4/5 justify-center'>
             <label htmlFor="coverLetter" className='font-semibold text-gray-600 text-md ml-2 mb-3'>Cover Letter</label> <br />
             <textarea rows="6" className="p-2.5 w-full mb-6 text-sm text-gray-900 bg-gray-50 rounded-lg border border-blue-400" placeholder='Write a beautiful Cover letter explaining why you are the best fit for this role...'/>
-            <label htmlFor="projects" className='font-semibold text-gray-600 text-md ml-2 mb-3'>Assignment / Task</label> <br />
+            <label htmlFor="projects" className='font-semibold text-gray-600 text-md ml-2 mb-3'>Assignment/Github</label> <br />
             <textarea rows="6" className="p-2.5 w-full mb-6 text-sm text-gray-900 bg-gray-50 rounded-lg border border-blue-400" placeholder='Brief Description of the Assignment and its Link...'/>
             {/* <textarea rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Explain about all your relevant projects... \n1'/> */}
             <button type='submit' onClick={handleSubmit} className='bg-sky-400 text-white rounded-md px-10 py-1 text-lg mt-2 hover:bg-white hover:text-sky-400 hover:border-2 hover:border-sky-400'>Submit</button>
