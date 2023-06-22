3362a00848c40801e477359ca4fdaffa4d547e28
stupendo1238 <mohitagarwal4567@example.com>
2023-06-22 12:50:33 +0530

    mcq

--- app/mcqs/[id]/page.jsx
+++ app/mcqs/[id]/page.jsx
@@ -6,9 +6,14 @@ import { useState,useEffect } from "react";
 const fetcher = (url) => fetch("https://api.jsonbin.io/v3/b/649318cf8e4aa6225eb211a2").then((res) => res.json());
 const page = ({id}) => {
   const [que,setQue]=useState([])
-  const [color,setColor]=useState("")
-  function handleClick(index){
-    (index+1)===1?setColor("green"):setColor("red")
+  const [current,setCurrent]=useState("")
+  const [message1,setMessage1]=useState(false)
+  const [message2,setMessage2]=useState(false)
+  function handleClick(position,index,ans){
+    console.log(position)
+    setCurrent(position);
+    index+1===ans? setMessage1(true):setMessage2(true)
+    console.log(current)
   }
   const { data, error } = useSWR( "/api/mcq/", fetcher);
   useEffect(()=>{
@@ -18,26 +23,36 @@ const page = ({id}) => {
     console.log(quest)
     setQue(quest[0])
   },[data])
+  useEffect(()=>{
+    setTimeout(()=>{
+      message1?setMessage1(false):""
+      message2?setMessage2(false):""
+    },1000)
+  })
   // ... handle loading and error states
   if (error) return <div>Failed to loadinggggggg job data</div>;
   if (!data) return <div>Loading...</div>;
   console.log(data.record)
   return (
     <div className="mb-20 w-screen">
-    <p className="text-4xl text-center text-white text mt-8 mb-12">{`Top ${id} MCQ's`}</p>
-    <div className="text-white mx-32">
+    <p className="text-4xl text-center text-white text mt-8 mb-12">{`Top ${data.record[0]._id} MCQ's`}</p>
+    <div>
       {que&&<div>{que.map((ele,i)=>{
         return(
-          <>
-          <div className="text-left text-xl text">{`${i+1}) ${ele.question}`}</div>
-          <div className="flex flex-col mt-4 mb-6">{ele.options.map((item,j)=>{
+          <div key={i}>
+          <div className="question text-center bg-sky-300 rounded-2xl text">{`${i+1}. ${ele.question}`}</div>
+          <div className="flex flex-col mt-4 mb-2">{ele.options.map((item,j)=>{
             return(
-              <button onClick={()=>handleClick(j)} className="text-left ml-6 mb-1 px-10 py-2 w-1/3">
+              <button onClick={()=>handleClick(i,j,ele.answer)} className="options rounded-2xl text text-sm text-center bg-white text-black mb-2 hover:bg-yellow-100">
                 {item}
               </button>
             )
           })}</div>
-          </>
+          <div className="h-20 mb-2">
+          {current===i && message1 && <div className="result rounded-md text-center text-black bg-green-300">Yayy!! Correct Answer.</div>}
+          {current===i && message2 && <div className="result rounded-md text-center text-black bg-red-300">Oops!! Wrong Answer. Please try again.</div>}
+          </div>
+          </div>
         )
       })}</div>}
     </div>
