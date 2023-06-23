3a01e7e1517284a61a2facbb77759b4436257fb5
stupendo1238 <mohitagarwal4567@example.com>
2023-06-23 13:44:02 +0530

    Testimonials

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,77 +1,99 @@
 "use client"
+import { useState,useEffect,useRef } from "react";
 import { Card, Grid, Row, Text } from "@nextui-org/react";
-
+import SimpleImageSlider from "react-simple-image-slider";
 
 const Testimonial = () => {
+  const [index,setIndex]=useState(0)
+  const timeoutRef = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(() =>
+        setIndex((prevIndex) =>
+          prevIndex === list.length - 1 ?0 : prevIndex + 1
+        ),2500);
+     return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
     const list = [
         {
           name: "Ajay Jeena",
           title:"Student at NIT Raipur",
-          img: "/assets/images/att.jpg",
+          img: "/assets/images/image4.png",
           desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
 
         },
         {
             name: "Raunit Verma",
             title:"Student at NIT Raipur",
-          img: "/assets/images/att.jpg",
+          img: "/assets/images/image1.png",
           desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
         },
         {
             name: "Raunit Verma",
             title:"Student at NIT Raipur",
-          img: "/assets/images/att.jpg",
+          img: "/assets/images/image2.png",
           desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
         },
         {
             name: "Ajay Jeena",
             title:"Student at NIT Raipur",
-          img: "/assets/images/att.jpg",
+          img: "/assets/images/image3.png",
+          desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
+        },
+        {
+          name: "Ajay Jeena",
+          title:"Student at NIT Raipur",
+          img: "/assets/images/image4.png",
+          desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
+        },
+        {
+          name: "Ajay Jeena",
+          title:"Student at NIT Raipur",
+          img: "/assets/images/image1.png",
+          desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
+
+        },
+        {
+            name: "Ajay Jeena",
+            title:"Student at NIT Raipur",
+          img: "/assets/images/guess.png",
           desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
         },
         {
             name: "Ajay Jeena",
             title:"Student at NIT Raipur",
-          img: "/assets/images/att.jpg",
-          desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
-        },
-        {
-            name: "Ajay Jeena",
-            title:"Student at NIT Raipur",
-          img: "/assets/images/att.jpg",
-          desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
-        },
-        {
-            name: "Ajay Jeena",
-            title:"Student at NIT Raipur",
-          img: "/assets/images/att.jpg",
-          desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
-        },
-        {
-            name: "Ajay Jeena",
-            title:"Student at NIT Raipur",
-          img: "/assets/images/att.jpg",
+          img: "/assets/images/pi.png",
           desc: "I am very thankful to talent connect for their such beautiful courses these courses are really helpful"
         },
       ];
   return (
       
-        <div className="containerslider w-screen">
-            <div className="cards-container">
+        <div className="w-full slideshow">
+            <div style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }} className="slideshowSlider">
               {/* <div > */}
-            {list.map((item, index) => (
-        <div className="card">
-          <Card isHoverable >
+            {list.map((item, i) => {
+            return(<div className="slide">
+          <Card>
             <Card.Body css={{ p: 0 }}>
               <Card.Image
                 src={item.img}
-                objectFit="fit"
+                objectFit="contain"
                 width="100%"
-                height={200}
+                height={280}
                 alt={item.title}
               />
             </Card.Body>
-            <Card.Footer css={{ justifyItems: "flex-start" }}>
+            <Card.Footer css={{ justifyItems: "flex-start",padding:"$8",backgroundColor:"SkyBlue",color:"WhiteSmoke" }}>
               <Row wrap="wrap" justify="space-between" align="center">
                 <Text b>{item.name}</Text>
                 <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
@@ -81,10 +103,21 @@ const Testimonial = () => {
             </Card.Footer>
           </Card>
           {/* <img src={item.img}/> */}
-        </div>
-      ))}
+        </div>)
+    })}
             {/* </div> */}
             </div>
+        <div className="slideshowDots">
+        {list.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
         </div>
   )
 }
