fc2d4eadff9998fb2f9ce949e44a24271c2770ba
stupendo1238 <mohitagarwal4567@example.com>
2023-06-23 14:20:25 +0530

    Styles

--- app/hr/addjob/page.jsx
+++ app/hr/addjob/page.jsx
@@ -112,7 +112,6 @@ const page = ({params}) => {
               css={{mt:"$5",backgroundColor:"$white"}}
               rows='5'
               fullWidth="true"
-              
               placeholder="Eligibility Criteria"
               required
             />
@@ -130,7 +129,7 @@ const page = ({params}) => {
               <div className="flex flex-col mx-auto w-full">
               <div className="flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill: "white",marginTop:"1.6vh",marginLeft:"0.6em"}}><path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path><path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path></svg>
-              <label className="text-sm my-3 ml-1">Expected Start Date</label>
+              <label className="text-sm text-white my-3 ml-1">Expected Start Date</label>
               </div>
               <input className="rounded-md p-2" type="date" css={{backgroundColor:"$white"}} clearable  Placeholder="Expected Start Date" initialValue=""/>
               </div>
