12604619deb8c8584dc799acd27226de4596538f
stupendo1238 <mohitagarwal4567@example.com>
2023-06-26 14:51:19 +0530

    Addjob

--- app/hr/addjob/page.jsx
+++ app/hr/addjob/page.jsx
@@ -19,7 +19,8 @@ const page = ({params}) => {
       e.preventDefault();
       // setIsSubmitting(true);
       console.log("Form Submitted");
-      e.target[0].value && e.target[2].value && e.target[4].value && e.target[8].value && e.target[13].value && e.target[15].value && e.target[17].value && e.target[18].value?setPosted(true):setPosted(false) 
+      setPosted(true)
+      // e.target[0].value && e.target[2].value && e.target[4].value && e.target[8].value && e.target[15].value && e.target[17].value && e.target[18].value?setPosted(true):setPosted(false) 
       try {
         for(let i=0; i<e.target.length; i++){
           console.log("index is this ",i," label is this ",e.target[i].label," value is this ",e.target[i].value);
@@ -57,13 +58,12 @@ const page = ({params}) => {
     };
     const [posted,setPosted]=useState(false)
     function handleSubmit(e){
-
     }     
   return (
     !posted?<>
-    <div className='justify-center w-full align-center justify-items-center hrjobs mt-16 mb-20'>
+    <div className='justify-center w-full align-center justify-items-center hrjobs mt-16 mb-20 border-2 border-white p-4'>
           <h1 className='font-bold text-5xl text-left mb-5'>
-            <p className='text text-4xl text-center text-white mb-8'>Post a Job and hire the best Talent</p>
+            <p className='textnew text-4xl text-center text-white mb-8'>Post a Job and hire the best Talent</p>
           </h1>
         <form onSubmit={createNewJob} className='w-full'>
           <Grid.Container gap={2} >
@@ -71,7 +71,7 @@ const page = ({params}) => {
               <div className="flex flex-col mx-auto w-full">
                 <div className="flex">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill:"white",marginTop:"1.8vh",marginLeft:"0.6em"}}><path d="M12 10c1.151 0 2-.848 2-2s-.849-2-2-2c-1.15 0-2 .848-2 2s.85 2 2 2zm0 1c-2.209 0-4 1.612-4 3.6v.386h8V14.6c0-1.988-1.791-3.6-4-3.6z"></path><path d="M19 2H5c-1.103 0-2 .897-2 2v13c0 1.103.897 2 2 2h4l3 3 3-3h4c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm-5 15-2 2-2-2H5V4h14l.002 13H14z"></path></svg> 
-                <label className="text-sm text-white my-3 ml-1">Job Title</label>
+                <label className="text-sm text-white my-3 ml-1">Job Title *</label>
                 </div>
               <input className="rounded-md p-2" css={{backgroundColor:"$white"}} clearable placeholder="Job Title" initialValue="" required/>
               </div>
@@ -80,7 +80,7 @@ const page = ({params}) => {
               <div className="flex flex-col mx-auto w-full">
               <div className="flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill: "white",marginTop:"1.6vh",marginLeft:"0.6em"}}><path d="M21 4H3a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1zm-1 11a3 3 0 0 0-3 3H7a3 3 0 0 0-3-3V9a3 3 0 0 0 3-3h10a3 3 0 0 0 3 3v6z"></path><path d="M12 8c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4-1.794-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z"></path></svg>
-              <label className="text-sm text-white my-3 ml-1">Stipend</label>
+              <label className="text-sm text-white my-3 ml-1">Salary *</label>
               </div>
               <input className="rounded-md p-2" css={{backgroundColor:"$white"}} clearable  placeholder="Stipend" initialValue="" required/>
               </div>
@@ -89,7 +89,7 @@ const page = ({params}) => {
               <div className="flex flex-col mx-auto w-full">
               <div className="flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill:"white",marginTop:"1.6vh",marginLeft:"0.6em"}}><path d="M18 2H6c-1.103 0-2 .897-2 2v17a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4c0-1.103-.897-2-2-2zm0 18H6V4h12v16z"></path><path d="M8 6h3v2H8zm5 0h3v2h-3zm-5 4h3v2H8zm5 .031h3V12h-3zM8 14h3v2H8zm5 0h3v2h-3z"></path></svg>
-              <label className="text-sm text-white my-3 ml-1">Company Name</label>
+              <label className="text-sm text-white my-3 ml-1">Company Name *</label>
               </div>
               <input className="rounded-md p-2" type="text" css={{backgroundColor:"$white"}} clearable  placeholder="Company Name" initialValue="" required/>
               </div>
@@ -106,7 +106,7 @@ const page = ({params}) => {
             <div className="flex flex-col mx-auto w-full">
             <div className="flex">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill: "white",marginTop:"1.6vh",marginLeft:"0.6em"}} ><path d="m2.394 13.742 4.743 3.62 7.616-8.704-1.506-1.316-6.384 7.296-3.257-2.486zm19.359-5.084-1.506-1.316-6.369 7.279-.753-.602-1.25 1.562 2.247 1.798z"></path></svg>
-            <label className="text-sm text-white mt-3 ml-1">Eligibility</label>
+            <label className="text-sm text-white mt-3 ml-1">Eligibility *</label>
             </div>
             <Textarea
               css={{mt:"$5",backgroundColor:"$white"}}
@@ -120,9 +120,9 @@ const page = ({params}) => {
               <div className="flex flex-col mx-auto w-full">
               <div className="flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill: "white",marginTop:"1.6vh",marginLeft:"0.6em"}}><path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path><path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path></svg>
-              <label className="text-sm text-white my-3 ml-1">Last Date to Apply</label>
+              <label className="text-sm text-white my-3 ml-1">Last Date to Apply *</label>
               </div>
-              <input className="rounded-md p-2" type="date" css={{backgroundColor:"$white"}} clearable  Placeholder="Last Date to apply" initialValue=""/>
+              <input className="rounded-md p-2" type="date" css={{backgroundColor:"$white"}} clearable  Placeholder="Last Date to apply" initialValue="" required/>
               </div>
             </Grid>
             <Grid xs={12} sm={4}>
@@ -138,7 +138,7 @@ const page = ({params}) => {
               <div className="flex flex-col mx-auto w-full">
               <div className="flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill: "white",marginTop:"1.6vh",marginLeft:"0.6em"}}><path d="M16.604 11.048a5.67 5.67 0 0 0 .751-3.44c-.179-1.784-1.175-3.361-2.803-4.44l-1.105 1.666c1.119.742 1.8 1.799 1.918 2.974a3.693 3.693 0 0 1-1.072 2.986l-1.192 1.192 1.618.475C18.951 13.701 19 17.957 19 18h2c0-1.789-.956-5.285-4.396-6.952z"></path><path d="M9.5 12c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2zm1.5 7H8c-3.309 0-6 2.691-6 6v1h2v-1c0-2.206 1.794-4 4-4h3c2.206 0 4 1.794 4 4v1h2v-1c0-3.309-2.691-6-6-6z"></path></svg>
-              <label className="text-sm text-white my-3 ml-1">No. of Openings</label>
+              <label className="text-sm text-white my-3 ml-1">No. of Openings *</label>
               </div>
               <input className="rounded-md p-2" type="number" css={{backgroundColor:"$white"}} clearable  Placeholder="No. of Openings" initialValue="" required/>
               </div>
@@ -147,7 +147,7 @@ const page = ({params}) => {
               <div className="flex flex-col mx-auto w-full">
               <div className="flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill:"white",marginTop:"1.6vh",marginLeft:"0.6em"}}><path d="M5 22h14c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2h-2a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1H5c-1.103 0-2 .897-2 2v15c0 1.103.897 2 2 2zM5 5h2v2h10V5h2v15H5V5z"></path><path d="m11 13.586-1.793-1.793-1.414 1.414L11 16.414l5.207-5.207-1.414-1.414z"></path></svg>
-              <label className="text-sm text-white my-3 ml-1">Skills Required</label>
+              <label className="text-sm text-white my-3 ml-1">Skills Required *</label>
               </div>
               <input className="rounded-md p-2" type="text" css={{backgroundColor:"$white"}} clearable  Placeholder="Skills" initialValue="" required/>
               </div>
@@ -155,7 +155,7 @@ const page = ({params}) => {
             <div className="flex flex-col mx-auto w-full">
             <div className="flex">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill: "white",marginTop:"4.7vh",marginLeft:"0.6em"}}><path d="M20 6h-3V4c0-1.103-.897-2-2-2H9c-1.103 0-2 .897-2 2v2H4c-1.103 0-2 .897-2 2v11c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2zm-4 2v11H8V8h8zm-1-4v2H9V4h6zM4 8h2v11H4V8zm14 11V8h2l.001 11H18z"></path></svg>
-            <label className="text-sm text-white mt-8 ml-1">Job Description</label>
+            <label className="text-sm text-white mt-8 ml-1">Job Description *</label>
             </div>
             <Textarea
               css={{mt:"$5",backgroundColor:"$white"}}
@@ -169,7 +169,7 @@ const page = ({params}) => {
             <div className="flex flex-col mx-auto w-full">
             <div className="flex">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill: "white",marginTop:"4.7vh",marginLeft:"0.6em"}}><path d="M19 4h-3V2h-2v2h-4V2H8v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zM5 20V7h14V6l.002 14H5z"></path><path d="M7 9h10v2H7zm0 4h5v2H7z"></path></svg>
-            <label className="text-sm text-white mt-8 ml-1">Responsibilities</label>
+            <label className="text-sm text-white mt-8 ml-1">Responsibilities *</label>
             </div>
             <Textarea
               css={{mt:"$5",backgroundColor:"$white"}}
@@ -193,13 +193,13 @@ const page = ({params}) => {
               label="Description of the Job"
               placeholder="Description of the Job"
             /> */}
-          <button onClick={handleSubmit} type='submit' className='py-2 px-10 rounded-xl bg-white ml-6 mt-4 border-sky-800 border-2 hover:bg-white hover:text-blue-500 hover:border-blue-600'  >Submit</button>
+          <button type='submit' className='py-2 px-10 rounded-xl bg-white ml-6 mt-4 border-sky-800 border-2 hover:bg-white hover:text-blue-500 hover:border-blue-600'  >Submit</button>
         </form>
     </div>
     </>:<div className="flex flex-col justify-center items-center mb-20">
       <img width="250" src="/assets/images/jobpost.gif"></img>
       <p className="text-4xl text text-lime-500 mb-4">Successfully published the job.</p>
-      <p>Check the dashboard for tracking the application.</p>
+      <p className="text-white">Check the dashboard for tracking the application.</p>
     </div>
   )
 }
