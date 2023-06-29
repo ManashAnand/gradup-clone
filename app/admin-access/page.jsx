4b65680f0dd5bcafb614cadc1a2e07cbb72808fb
stupendo1238 <mohitagarwal4567@example.com>
2023-06-29 12:25:14 +0530

    Admin

--- app/admin-access/page.jsx
+++ app/admin-access/page.jsx
@@ -12,24 +12,43 @@ const page = () => {
       if (!data) return <div className="my-60"><Spinner/></div>;
       console.log(data);
   return (
-    <div>
-        <div>
-            Total Users are - {data.totalNoOfUsers}
+    <div className="text-white my-20 flex flex-col items-center justify-center">
+        <div className="flex justify-center m-auto">
+        <div className="px-4 py-2 rightmargin">
+        <p className="text-center textnew">Total HR's</p>
+        <p className="text-center text-6xl overflow-hidden my-auto animate-charcter">{data.totalHRs}</p>
+            {/* Out of which HR's are -  {data.totalHRs} */}
         </div>
-        <div>
-            Out of which HR's are -  {data.totalHRs}
+        <div className="bg-green-500 ml-8 circlepad1 rounded-full">
+        <p className="text-center textnew">Total Users</p>
+        <p className="text-center text-5xl overflow-hidden">{data.totalNoOfUsers}</p>
         </div>
-        <div>
-            Total Alumni are - {data.totalAlumni}
+        <div className="px-4 py-2 leftmargin">
+        <p className="text-center textnew">Total Alumni's</p>
+        <p className="text-center text-6xl overflow-hidden animate-charcter">{data.totalAlumni}</p>
+            {/* Total Alumni are - {data.totalAlumni} */}
         </div>
-        <div>
-            Last 7 day registration are - {data.lastSevenDaysRegisteresUsers}
         </div>
-        <div>
-            Total Jobs Posted - {data.totalJobs} 
+        <div className="px-16 py-4 bg-green-500 circlepad3 mt-20 rounded-md">
+        <p className="text-center textnew">Last 7 days registration</p>
+        <p className="text-center text-5xl overflow-hidden">{data.lastSevenDaysRegisteresUsers}</p>
+            {/* Last 7 day registration are - {data.lastSevenDaysRegisteresUsers} */}
         </div>
-        <div>
-            Total Active Jobs - {data.totalActiveJobs}
+        <div className="flex mt-20">
+        <div className="px-4 py-2 rightmargin">
+        <p className="text-center textnew">Active Jobs</p>
+        <p className="text-center text-6xl overflow-hidden my-auto animate-charcter">{data.totalActiveJobs}</p>
+            {/* Out of which HR's are -  {data.totalHRs} */}
+        </div>
+        <div className="bg-green-500 ml-4 circlepad2 rounded-full">
+        <p className="text-center textnew">Total Jobs</p>
+        <p className="text-center text-5xl overflow-hidden">{data.totalJobs}</p>
+        </div>
+        <div className="px-4 py-2 leftmargin">
+        <p className="text-center textnew">Inactive Jobs</p>
+        <p className="text-center text-6xl overflow-hidden animate-charcter">{data.totalJobs-data.totalActiveJobs}</p>
+            {/* Total Alumni are - {data.totalAlumni} */}
+        </div>
         </div>
     </div>
   )
