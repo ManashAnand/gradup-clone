ac3186c3eab163472090eb36106e5f0d867be9dd
stupendo1238 <mohitagarwal4567@example.com>
2023-07-10 19:08:00 +0530

    data

--- app/problem-statement/page.jsx
+++ app/problem-statement/page.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import styles from 'styles/Page.module.css'
 const page = () => {
   return (
-    <div className='w-screen mb-6'>
-      <p className='text-4xl font-semibold textnew text-white text-center mt-14'>Solution to the Problem statement !!</p>
-    <div className={styles.content}>
-      <h1 className='text-green-300 text-left text-3xl mt-6 mb-8'>Challenges of Finding Quality Internships</h1>
+    <div className='w-screen px-10 pb-20 headingbox2'>
+      <p className='text-4xl font-semibold textnew text-white text-center mt-14'>Solution to the Problem !!</p>
+    <div className="">
+      <h1 className='text-green-300 justice text-3xl mt-10 mb-8'>Challenges of Finding Quality Internships</h1>
       <p className='text-white justice textnew'>Finding quality internships is a major challenge for students in
       India. Many students struggle to find internships that suit their
       skills and interests, leading to frustration and missed
@@ -19,7 +19,7 @@ const page = () => {
       process, making it easier for students to find quality
       internships and businesses to find the right interns.</p>
 
-      <h1 className='text-green-300 text-left text-3xl mt-12 mb-8'>Simplifying the
+      <h1 className='text-green-300 justice text-3xl mt-12 mb-8'>Simplifying the
       Internship Process</h1>
       <p className='text-white justice textnew'>GradUp is a comprehensive platform that simplifies the internship
       process for both students and businesses. With our user-friendly
