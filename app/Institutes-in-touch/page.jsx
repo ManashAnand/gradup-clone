aff0c37ef6d7d318e2d5d063931a39a72734d4a1
Ajayjeena <ajayjeena20@gmail.com>
2023-07-12 02:47:59 +0530

    Added institutes in touch

--- app/Institutes-in-touch/page.jsx
+++ app/Institutes-in-touch/page.jsx
@@ -3,7 +3,62 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <img className='mt-12' src="/assets/achievements/Institutes-in-touch.jpeg"></img>
+      IIM Bodhgaya
+      Hansraj college, DU
+      Shaheed Rajguru college, DU
+      Sri Guru Gobind Singh College, DU 
+      PG DAV College, DU
+      NIT Jamshedpur
+      IIT Kharagpur 
+      IIM Kafhipur 
+      IIM Sirmaur 
+      IIM Indore 
+      Dr. DY Patil Pune 
+      BITS Pilani (Hyderabad campus)
+      Bundelkhand University Jhansi 
+      Rajdhani College, DU 
+      KL University, Hyderabad 
+      Ramjas College, DU 
+      GNIOT Greater Noida 
+      AIESEC ,DU
+      NIT Sikkim
+      Shaheed Sukhdev College of Business Studies 
+      XLRI Jamshedpur 
+      BIT Mesra 
+      Aatma Ram Sanatam college, DU 
+      Xt Xavier College, Kolkata 
+      IIM Rohtak 
+      IIM Trichy 
+      LBSIM Delhi 
+      SIDTM Pune 
+      NMIMS Mumbai 
+      IIT Kanpur 
+      SOIL Gurgaon 
+      IIT Jodhpur 
+      BIIB Pune 
+      Delhi School of Economics 
+      PUMBA 
+      Jamia Milia Islamia 
+      IIT Dhanbad 
+      Christ University Banglore 
+      IIT Delhi 
+      NIT Agartala 
+      IIIT Una 
+      IIIT Gwalior 
+      MNIT Allahabad 
+      Faculty of Social Sciences BHU 
+      IMI Bhubneswar 
+      Symbiosis Noida 
+      Motilal Nehru College ,DU 
+      Kirorimal College, DU 
+      NMIMS Hyderabad 
+      Delhi Technical University 
+      IIM Ranchi 
+      IIM Udaipur 
+      IIM Vishakhapatnam 
+      NIT Hamirpur 
+      NIT Karnataka 
+      Ponducherry University 
     </div>
   )
 }
