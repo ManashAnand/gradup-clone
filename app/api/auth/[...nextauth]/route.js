f5c34cd8b520122af4864e5ee30ce16f602c3d5d
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-08-02 14:31:19 +0530

    commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -20,6 +20,7 @@ const handler = NextAuth({
         TwitterProvider({
             clientId: process.env.TWITTER_CLIENT_ID,
             clientSecret: process.env.TWITTER_CLIENT_SECRET,
+            version: "2.0"
         }),
         // Providers.Credentials({
         //     name: 'Credentials',
