1d058bca7eeb221f3866d3d6e5610f00bcee6901
Diyansh Rao <87599592+DiyanshRao@users.noreply.github.com>
2023-09-21 23:27:06 +0530

    env changes

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -10,13 +10,11 @@ export async function GET(req) {
     const email = url.searchParams.get('email')
 
     const cartData = await Cart.find({ userId: email })
-
     const cartIds = cartData.map((cart) => {
-      // Manually convert _id to a string
       const courseIdString = cart.courseId.toString()
       return courseIdString
     })
-    console.log(cartIds)
+
     const coursesData = await Courses.find({ _id: { $in: cartIds } })
       .select('_id imageURL price title author')
       .populate('author')
